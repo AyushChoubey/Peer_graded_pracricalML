a19b5a7635da1b5750ef302e5ee8349ff1eb33fd
Ayush Kumar <choubey.ayush875@gmail.com>
2018-03-28 00:53:57 +0530

    plo3.R

---
+++ plot3.R
@@ -0,0 +1,13 @@
+data<-read.table("household_power_consumption.txt",header = TRUE,sep=";",stringsAsFactors = FALSE,dec =".")
+udata<-data[data$Date %in% c("1/2/2007","2/2/2007")]
+dateandtime<-strptime(paste(udata$Date,udata$Time,sep=" "),format = "%d/%m/%Y %H:%M:%S")
+udata$Global_active_power<-as.numeric(udata$Global_active_power)
+udata$Sub_metering_1<-as.numeric(udata$Sub_metering_1)
+udata$Sub_metering_2<-as.numeric(udata$Sub_metering_2)
+udata$Sub_metering_3<-as.numeric(udata$Sub_metering_3)
+png("plot3.png",width = 480,height = 480)
+plot(dateandtime,udata$Sub_metering_1,xlab = " ",ylab = "Energy sub mettering",type = "l")
+lines(dateandtime,udata$Sub_metering_2,type = "l",col="red")
+lines(dateandtime,udata$Sub_metering_3,type = "l",col="blue")
+legend("topright",pch ="-",col = c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
